0a0d91cdfe952809af7ecb750eedac7de729985e
Anastasiia Yermakova <yermak1037@gmail.com>
2020-11-24 14:15:52 +0200

    init: start homework 1 task 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from './components/Profile';
+import user from './data/user.json';
+
+function App() {
+  return (
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  );
+}
+
+export default App;
